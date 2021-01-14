307ae5527ac0f4dcc87ebf25c7bc1811f305e7a1
revolist <maksim.k@revolist.eu>
2021-01-14 12:35:36 -0500

    updates

--- framework/svelte/src/revodropdown.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-/* tslint:disable */
-// @ts-nocheck
-import '@revolist/revo-dropdown/loader';
-
-import RevoDropdown from './components/RevoDropdown';
-import RevoList from './components/RevoList';
-export { RevoDropdown };
-export { RevoList };
