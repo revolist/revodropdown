13c926b05e7c66ebdfa85842cbed6e10c1a7fb19
maks <kudrytskimaksim@gmail.com>
2026-03-28 21:34:09 +0000

    Updates

--- build/item.helpers-a730f109.js
+++
@@ -1,8 +0,0 @@
-function getItemLabel(item, dataLabel) {
-  return dataLabel ? item[dataLabel] : item;
-}
-function getItemValue(item, dataId) {
-  return dataId ? item[dataId] : item;
-}
-
-export { getItemValue as a, getItemLabel as g };
